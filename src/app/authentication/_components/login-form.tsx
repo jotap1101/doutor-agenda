7f092e003cb340204b935f5a9cff56898aec2cbf
jotap1101 <jotap1101.joaopedro@gmail.com>
2025-06-07 21:07:25 -0300

    feat: implement authentication schema and forms with better-auth integration

--- src/app/authentication/_components/login-form.tsx
+++ src/app/authentication/_components/login-form.tsx
@@ -22,31 +22,25 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
-const registerSchema = z.object({
-  name: z.string().trim().min(1, { message: "Nome é obrigatório" }),
+const loginSchema = z.object({
   email: z
     .string()
     .trim()
     .min(1, "Email é obrigatório")
     .email("Email inválido"),
-  password: z
-    .string()
-    .trim()
-    .min(1, { message: "Senha é obrigatória" })
-    .min(6, { message: "Senha deve ter pelo menos 6 caracteres" }),
+  password: z.string().trim().min(1, { message: "Senha é obrigatória" }),
 });
 
-const SignUpForm = () => {
-  const form = useForm<z.infer<typeof registerSchema>>({
-    resolver: zodResolver(registerSchema),
+const LoginForm = () => {
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
     defaultValues: {
-      name: "",
       email: "",
       password: "",
     },
   });
 
-  function onSubmit(values: z.infer<typeof registerSchema>) {
+  function onSubmit(values: z.infer<typeof loginSchema>) {
     console.log(values);
   }
 
@@ -55,26 +49,12 @@ const SignUpForm = () => {
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
           <CardHeader>
-            <CardTitle>Criar conta</CardTitle>
-            <CardDescription>Crie sua conta aqui.</CardDescription>
+            <CardTitle>Fazer login</CardTitle>
+            <CardDescription>
+              Faça login na sua conta para continuar.
+            </CardDescription>
           </CardHeader>
           <CardContent className="grid gap-6">
-            <FormField
-              control={form.control}
-              name="name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Nome</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Digite seu nome" {...field} />
-                  </FormControl>
-                  {/* <FormDescription>
-                            Este é o seu nome público.
-                          </FormDescription> */}
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
             <FormField
               control={form.control}
               name="email"
@@ -114,7 +94,7 @@ const SignUpForm = () => {
           </CardContent>
           <CardFooter>
             <Button type="submit" className="w-full">
-              Criar conta
+              Entrar
             </Button>
           </CardFooter>
         </form>
@@ -123,4 +103,4 @@ const SignUpForm = () => {
   );
 };
 
-export default SignUpForm;
+export default LoginForm;
--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,15 +1,5 @@
-"use client";
-
+import LoginForm from "@/app/authentication/_components/login-form";
 import SignUpForm from "@/app/authentication/_components/sign-up-form";
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const AuthenticationPage = () => {
@@ -17,23 +7,12 @@ const AuthenticationPage = () => {
     <div className="flex h-screen w-screen items-center justify-center">
       <div className="flex w-full max-w-sm flex-col gap-6">
         <Tabs defaultValue="login">
-          <TabsList>
+          <TabsList className="w-full">
             <TabsTrigger value="login">Login</TabsTrigger>
             <TabsTrigger value="register">Criar conta</TabsTrigger>
           </TabsList>
           <TabsContent value="login">
-            <Card>
-              <CardHeader>
-                <CardTitle>Login</CardTitle>
-                <CardDescription>
-                  Faça login na sua conta para continuar.
-                </CardDescription>
-              </CardHeader>
-              <CardContent className="grid gap-6"></CardContent>
-              <CardFooter>
-                <Button>Entrar</Button>
-              </CardFooter>
-            </Card>
+            <LoginForm />
           </TabsContent>
           <TabsContent value="register">
             <SignUpForm />
