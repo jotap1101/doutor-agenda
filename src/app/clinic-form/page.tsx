a57ac29adba25b386b0797eed7920f9c8a37c708
jotap1101 <jotap1101.joaopedro@gmail.com>
2025-06-08 21:40:27 -0300

    feat: add clinic creation functionality with form and dialog components; update database schema integration

---
+++ src/app/clinic-form/page.tsx
@@ -0,0 +1,26 @@
+import ClinicForm from "@/app/clinic-form/components/form";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+const ClinicFormPage = () => {
+  return (
+    <Dialog open>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Adicionar clínica</DialogTitle>
+          <DialogDescription>
+            Adicione uma clínica para continuar.
+          </DialogDescription>
+        </DialogHeader>
+        <ClinicForm />
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default ClinicFormPage;
